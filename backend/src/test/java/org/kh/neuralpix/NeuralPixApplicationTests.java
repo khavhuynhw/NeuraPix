32f944f28a236fa371299a22e840d177ae254d83
khavhuynhw <115153974+khavhuynhw@users.noreply.github.com>
2025-05-27 19:11:32 +0700

    Initial Commit

---
+++ backend/src/test/java/org/kh/neuralpix/NeuralPixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kh.neuralpix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuralPixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
