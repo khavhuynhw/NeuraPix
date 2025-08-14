95c8641c04cd6beadd502e9e6465fa4575dc6d93
khavhuynhw <115153974+khavhuynhw@users.noreply.github.com>
2025-08-15 02:45:25 +0700

    Update LoginPage.tsx

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -29,7 +29,7 @@ export const LoginPage = () => {
       form.setFields([
         {
           name: "email",
-          errors: [error.message || "Invalid email or password"],
+          errors: ["Invalid email or password"],
         },
       ]);
     }
